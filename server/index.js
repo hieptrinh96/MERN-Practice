2842aafc0a224b3bf2f16fd59a619911d16a96d6
hieptrinh96 <xmikecolx@gmail.com>
2023-05-29 15:22:12 -0500

    add router set up for user and auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+import {register} from './controllers/auth.js'
+
 
 // Configurations for middleware
 const __filename = fileURLToPath(import.meta.url)
@@ -32,11 +36,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage})
 
+// routes with files
+app.post('/auth/register', upload.single('picture'), register)
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
 // Mongoose setup
 const PORT = process.env.PORT || 6001
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  app.listen(PORT, () => console.log(`Server Post: ${PORT}`))
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 }).catch((error) => console.log(`${error}, did not connect`))
+
